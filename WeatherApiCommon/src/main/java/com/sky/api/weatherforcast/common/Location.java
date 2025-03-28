9b08fbdd0db1435a463db51220db505c9f32a442
Habibur Rahman <habibur0199mhr@gmail.com>
2025-03-28 12:52:12 +0600

    initial-commit

---
+++ WeatherApiCommon/src/main/java/com/sky/api/weatherforcast/common/Location.java
@@ -0,0 +1,5 @@
+package com.sky.api.weatherforcast.common;
+
+public class Location {
+
+}
---
+++ WeatherApiCommon/src/main/java/com/sky/api/weatherforcast/common/WeatherApiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.sky.api.weatherforcast.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiCommonApplication.class, args);
+	}
+
+}
